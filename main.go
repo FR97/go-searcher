58eb26dc960a24e8ac2ec6cfbb8dff9afe19a12b
FR97 <fristic97@gmail.com>
2023-09-14 21:31:16 +0200

    read file.

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import "os"
+
 func main() {
-	println("Hello, World!")
+
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		println("File path not provided")
+		return
+	}
+
+	filePath := args[0]
+
+	bytes, err := os.ReadFile(filePath)
+
+	if err != nil {
+		println("error reading a file: ", err.Error())
+		return
+	}
+
+	str := string(bytes)
+	println(str)
 }
