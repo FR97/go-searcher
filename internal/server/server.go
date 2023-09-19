cc5470d54d18b09fc7ca96c6f12307dd8bd3c96e
FR97 <fristic97@gmail.com>
2023-09-19 23:16:11 +0200

    basic html server

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,10 +2,12 @@ package server
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/fr97/go-searcher/internal/config"
+	"html/template"
+	"net/http"
 )
+
+const HTML_TEMPLATES_PATH = "./public/view/"
 
 func Serve(cfg config.Config) {
 
@@ -17,16 +19,35 @@ func Serve(cfg config.Config) {
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	html := `<!DOCTYPE html>
-  <div>
-    Hello...
-  <div>
-  `
-	htmlOK(w, html)
+type response struct {
+	HasQuery bool
+	Results  searchResult
 }
 
-func htmlOK(w http.ResponseWriter, html string) {
+type searchResult struct {
+	FileName string
+	FilePath string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	fmt.Println("query:", query)
+	if query == nil || len(query) <= 0 {
+		fmt.Println("I m here1")
+		htmlOK(w, response{HasQuery: false}, "index.gohtml")
+	} else {
+		fmt.Println("I m here2")
+		htmlOK(w, response{HasQuery: true}, "index.gohtml")
+	}
+}
+
+func htmlOK(w http.ResponseWriter, data interface{}, tmplFile string) {
 	w.Header().Add("content-type", "html")
-	fmt.Fprint(w, html)
+
+	tmpl, err := template.ParseFiles(HTML_TEMPLATES_PATH + tmplFile)
+	if err != nil {
+		fmt.Println("failed to parse template:", err)
+	}
+
+	tmpl.Execute(w, data)
 }
